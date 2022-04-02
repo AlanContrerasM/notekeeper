66b843cda2bbae7a69ab16c9e2f6e9c73cd82d96
Alan Contreras <alanrcontrerasm@gmail.com>
2022-04-02 12:59:44 -0700

    Add HomePage on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notekeeper/views/login_view.dart';
+
+import 'firebase_options.dart';
 //On main we can out of the gate include the MaterialApp information
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,8 +12,40 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ));
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home")
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch(snapshot.connectionState){
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if(user?.emailVerified ?? false){
+                  print("You are a verified user");
+                }else{
+                  print("You need to verify your email first");
+                }
+                return const Text('Done');
+              default:
+                return const Text("Loading");
+            }
+          },
+          
+        ),
+    );
+  }
+}
 
